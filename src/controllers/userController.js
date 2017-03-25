2429904eb2dca169dc1bc1c312f90e6609ff9bd4
IIpocTo <allxf95@gmail.com>
2017-03-26 00:14:48 +0300

    Add client-side validation Remove boilerplate from SignupForm to new TextFieldGroup component

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,36 +1,4 @@
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
-
-function validateInput(data) {
-  const errors = {};
-
-  if (Validator.isEmpty(data.username)) {
-    errors.username = 'This field is required';
-  }
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'This field is required';
-  }
-  if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
-  }
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'This field is required';
-  }
-  if (Validator.isEmpty(data.passwordConfirmation)) {
-    errors.passwordConfirmation = 'This field is required';
-  }
-  if (!Validator.equals(data.password, data.passwordConfirmation)) {
-    errors.passwordConfirmation = 'Passwords must match';
-  }
-  if (Validator.isEmpty(data.timezone)) {
-    errors.timezone = 'This field is required';
-  }
-
-  return {
-    errors,
-    isValid: isEmpty(errors),
-  };
-}
+import validateInput from '../../shared/validations/signup';
 
 async function saveUser(req, res) {
   const { errors, isValid } = validateInput(req.body);
