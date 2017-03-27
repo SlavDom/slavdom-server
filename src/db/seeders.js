b1820bc91409ba49ab56a46ab1a0580ae15caf7c
George <eakarpov@yandex.ru>
2017-03-27 21:46:10 +0300

    Added new translations

--- src/db/seeders.js
+++ src/db/seeders.js
@@ -6,6 +6,34 @@ export const languageSeeder = [
         code: 'sign_in',
         result: 'Войдати',
       },
+      {
+        code: 'title',
+        result: 'SlavDom',
+      },
+      {
+        code: 'reg$email',
+        result: 'Поща',
+      },
+      {
+        code: 'reg$username',
+        result: 'Назвищо',
+      },
+      {
+        code: 'reg$password',
+        result: 'Ґадсло',
+      },
+      {
+        code: 'reg$password_confirmation',
+        result: 'Повторите ґадсло',
+      },
+      {
+        code: 'reg$join_us',
+        result: 'Пристѫпай до нашога сплåченства!',
+      },
+      {
+        code: 'sign_up',
+        result: 'Записати сѧ',
+      },
     ],
   },
   {
@@ -18,6 +46,67 @@ export const languageSeeder = [
       {
         code: 'sign_in',
         result: 'Sign in',
+      },
+      {
+        code: 'reg$email',
+        result: 'Email',
+      },
+      {
+        code: 'reg$username',
+        result: 'Username',
+      },
+      {
+        code: 'reg$password',
+        result: 'Password',
+      },
+      {
+        code: 'reg$password_confirmation',
+        result: 'Password confirmation',
+      },
+      {
+        code: 'reg$join_us',
+        result: 'Join our community!',
+      },
+      {
+        code: 'sign_up',
+        result: 'Sign up',
+      },
+    ],
+  },
+  {
+    code: 'is',
+    translations: [
+      {
+        code: 'title',
+        result: 'SlavDom',
+      },
+      {
+        code: 'sign_in',
+        result: 'Zalogati sę',
+      },
+      {
+        code: 'reg$email',
+        result: 'Pošta',
+      },
+      {
+        code: 'reg$username',
+        result: 'Ličen nazov',
+      },
+      {
+        code: 'reg$password',
+        result: 'Parola',
+      },
+      {
+        code: 'reg$password_confirmation',
+        result: 'Podtvrdženje parola',
+      },
+      {
+        code: 'reg$join_us',
+        result: 'Pristupi do našoj společnosti!',
+      },
+      {
+        code: 'sign_up',
+        result: 'Zapisati sę',
       },
     ],
   },
