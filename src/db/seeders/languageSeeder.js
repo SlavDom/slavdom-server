86ff7ea3ecbaa19f707cbb53643edc69be0c4f98
George <eakarpov@yandex.ru>
2017-04-19 14:46:20 +0300

    New seeds added

---
+++ src/db/seeders/languageSeeder.js
@@ -0,0 +1,243 @@
+let languageSeeder;
+export default languageSeeder = [
+  {
+    code: 'nsl',
+    translations: [
+      {
+        code: 'sign_in',
+        prefix: ['common'],
+        result: 'Войдати',
+      },
+      {
+        code: 'title',
+        prefix: ['common'],
+        result: 'SlavDom',
+      },
+      {
+        code: 'email',
+        prefix: ['reg', 'contact'],
+        result: 'Поща',
+      },
+      {
+        code: 'username',
+        prefix: ['reg'],
+        result: 'Назвищо',
+      },
+      {
+        code: 'password',
+        prefix: ['reg'],
+        result: 'Ґадсло',
+      },
+      {
+        code: 'password_confirmation',
+        prefix: ['reg'],
+        result: 'Повторите ґадсло',
+      },
+      {
+        code: 'join_us',
+        prefix: ['reg'],
+        result: 'Пристѫпай до нашога сплåченства!',
+      },
+      {
+        code: 'sign_up',
+        prefix: ['common', 'reg'],
+        result: 'Записати сѧ',
+      },
+      {
+        code: 'contact',
+        prefix: ['common'],
+        result: 'Поврåтна вѧзба',
+      },
+      {
+        code: 'read_more',
+        prefix: ['news'],
+        result: 'Читати далѣй',
+      },
+      {
+        code: 'next',
+        prefix: ['news'],
+        result: 'Слѣдна',
+      },
+      {
+        code: 'previous',
+        prefix: ['news'],
+        result: 'Предна',
+      },
+      {
+        code: 'theme',
+        prefix: ['contact'],
+        result: 'Ѳема',
+      },
+      {
+        code: 'message',
+        prefix: ['contact'],
+        result: 'Писмо',
+      },
+      {
+        code: 'send',
+        prefix: ['contact'],
+        result: 'Послати',
+      },
+    ],
+  },
+  {
+    code: 'en',
+    translations: [
+      {
+        code: 'title',
+        prefix: ['common'],
+        result: 'SlavDom',
+      },
+      {
+        code: 'sign_in',
+        prefix: ['common'],
+        result: 'Sign in',
+      },
+      {
+        code: 'email',
+        prefix: ['reg', 'contact'],
+        result: 'Email',
+      },
+      {
+        code: 'username',
+        prefix: ['reg'],
+        result: 'Username',
+      },
+      {
+        code: 'password',
+        prefix: ['reg'],
+        result: 'Password',
+      },
+      {
+        code: 'password_confirmation',
+        prefix: ['reg'],
+        result: 'Password confirmation',
+      },
+      {
+        code: 'join_us',
+        prefix: ['reg'],
+        result: 'Join our community!',
+      },
+      {
+        code: 'sign_up',
+        prefix: ['common', 'reg'],
+        result: 'Sign up',
+      },
+      {
+        code: 'contact',
+        prefix: ['common'],
+        result: 'Contact',
+      },
+      {
+        code: 'read_more',
+        prefix: ['news'],
+        result: 'Read more',
+      },
+      {
+        code: 'next',
+        prefix: ['news'],
+        result: 'Next',
+      },
+      {
+        code: 'previous',
+        prefix: ['news'],
+        result: 'Previous',
+      },
+      {
+        code: 'theme',
+        prefix: ['contact'],
+        result: 'Theme',
+      },
+      {
+        code: 'message',
+        prefix: ['contact'],
+        result: 'Message',
+      },
+      {
+        code: 'send',
+        prefix: ['contact'],
+        result: 'Send',
+      },
+    ],
+  },
+  {
+    code: 'is',
+    translations: [
+      {
+        code: 'title',
+        prefix: ['common'],
+        result: 'SlavDom',
+      },
+      {
+        code: 'sign_in',
+        prefix: ['common'],
+        result: 'Zalogati sę',
+      },
+      {
+        code: 'email',
+        prefix: ['reg', 'contact'],
+        result: 'Pošta',
+      },
+      {
+        code: 'username',
+        prefix: ['reg'],
+        result: 'Ličen nazov',
+      },
+      {
+        code: 'password',
+        prefix: ['reg'],
+        result: 'Parola',
+      },
+      {
+        code: 'password_confirmation',
+        prefix: ['reg'],
+        result: 'Podtvrdženje parola',
+      },
+      {
+        code: 'join_us',
+        prefix: ['reg'],
+        result: 'Pristupi do našoj společnosti!',
+      },
+      {
+        code: 'sign_up',
+        prefix: ['common', 'reg'],
+        result: 'Zapisati sę',
+      },
+      {
+        code: 'contact',
+        prefix: ['common'],
+        result: 'Contact',
+      },
+      {
+        code: 'read_more',
+        prefix: ['news'],
+        result: 'Read more',
+      },
+      {
+        code: 'next',
+        prefix: ['news'],
+        result: 'Next',
+      },
+      {
+        code: 'previous',
+        prefix: ['news'],
+        result: 'Previous',
+      },
+      {
+        code: 'theme',
+        prefix: ['contact'],
+        result: 'Theme',
+      },
+      {
+        code: 'message',
+        prefix: ['contact'],
+        result: 'Message',
+      },
+      {
+        code: 'send',
+        prefix: ['contact'],
+        result: 'Send',
+      },
+    ],
+  },
+];
