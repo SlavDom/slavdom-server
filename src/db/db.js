1622e2b1ae0903d4ca3f580ed94ac54424c2480d
George <eakarpov@yandex.ru>
2017-04-21 20:32:23 +0300

    Deleted unneccessary files

--- src/db/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/slavdom");
-exports.default = mongoose;
