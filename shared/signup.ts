292c4b58a14080d353db4220a701650cf3065138
George <eakarpov@yandex.ru>
2017-05-04 23:08:40 +0300

    Bug with fields fixed

--- shared/signup.ts
+++ shared/signup.ts
@@ -8,18 +8,20 @@ export default function signupValidation(data: UserSignupData, field: string|und
     errors.username = undefined;
     if (Validator.isEmpty(data.username.value)) {
       errors.username = "This field is required";
-    }
-    if (!Validator.isAlphanumeric(data.username.value)) {
-      errors.username = "Login can contain only english letters and numbers";
+    } else {
+      if (!Validator.isAlphanumeric(data.username.value)) {
+        errors.username = "Login can contain only english letters and numbers";
+      }
     }
   }
   if ((field === "email"  || typeof field === "undefined") && data.email.touched) {
     errors.email = undefined;
     if (Validator.isEmpty(data.email.value)) {
       errors.email = "This field is required";
-    }
-    if (!Validator.isEmail(data.email.value)) {
-      errors.email = "Email is invalid";
+    } else {
+      if (!Validator.isEmail(data.email.value)) {
+        errors.email = "Email is invalid";
+      }
     }
   }
   if ((field === "password"  || typeof field === "undefined") && data.password.touched) {
@@ -32,9 +34,10 @@ export default function signupValidation(data: UserSignupData, field: string|und
     errors.passwordConfirmation = undefined;
     if (Validator.isEmpty(data.passwordConfirmation.value)) {
       errors.passwordConfirmation = "This field is required";
-    }
-    if (!Validator.equals(data.password.value, data.passwordConfirmation.value)) {
-      errors.passwordConfirmation = "Passwords must match";
+    } else {
+      if (!Validator.equals(data.password.value, data.passwordConfirmation.value)) {
+        errors.passwordConfirmation = "Passwords must match";
+      }
     }
   }
   if ((field === "timezone"  || typeof field === "undefined") && data.timezone.touched) {
