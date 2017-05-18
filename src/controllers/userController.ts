78b87d6794365fb0709804f15788484c3c61edf1
IIpocTo <allxf95@gmail.com>
2017-05-18 21:30:21 +0300

    More proper validation checks

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -50,9 +50,11 @@ export default class UserController {
       if (user) {
         if (user.username === data.username) {
           validationResult.errors.username = "Sorry, this username has been taken";
+          validationResult.isValid = false;
         }
         if (user.email === data.email) {
           validationResult.errors.email = "Email is already registered";
+          validationResult.isValid = false;
         }
       }
       return validationResult;
